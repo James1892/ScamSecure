2ee95dc32299abb0016fe2d0d35c0cd32bb306d4
James1892 <jamesn1312@gmail.com>
2025-05-27 16:39:56 +0100

    starting home page

--- Screen/Config.py
+++ Screen/Config.py
@@ -2,5 +2,5 @@ from kivy.config import Config
 
 
 def apply_window_size():
-    Config.set("graphics", "width", "390")
-    Config.set("graphics", "height", "844")
+    Config.set("graphics", "width", "393")
+    Config.set("graphics", "height", "852")
